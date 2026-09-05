c0ee18727b3f96c9a68f473689b85e6a5e8b4527
Jakub Balys <kbalys444@gmail.com>
2026-09-05 18:33:14 +0200

    D: Added the convertion function from a TextNode to an HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,6 +17,26 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, None)
         node2 = TextNode("This is a text node", TextType.BOLD, "https://www.bootdev.dev")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_italic_bold_code(self):
+        node_italic = TextNode("This is a italic node", TextType.ITALIC)
+        node_bold = TextNode("This is a bold node", TextType.BOLD)
+        node_code = TextNode("This is a code node", TextType.CODE)
+        html_node_italic = text_node_to_html_node(node_italic)
+        html_node_bold = text_node_to_html_node(node_bold)
+        html_node_code = text_node_to_html_node(node_code)
+        self.assertEqual(html_node_italic.tag, "i")
+        self.assertEqual(html_node_italic.value, "This is a italic node")
+        self.assertEqual(html_node_bold.tag, "b")
+        self.assertEqual(html_node_bold.value, "This is a bold node")
+        self.assertEqual(html_node_code.tag, "code")
+        self.assertEqual(html_node_code.value, "This is a code node")
     
 
 if __name__ == "__main__":
