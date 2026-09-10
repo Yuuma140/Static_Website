8ce9e081d03264dd646d96b94086aae5977e2f87
Jakub Balys <kbalys444@gmail.com>
2026-09-10 19:29:58 +0200

    J: added BlockType Enum class and block to block type function and test cases

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,51 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unorder_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+    count = 0
+    i = 1
+    is_quote = True
+    is_ul = True
+    is_ol = True
+    for char in block:
+        if char == "#":
+            count += 1
+        else:
+            break
+    if count > 0 and count < 7 and block[count] == " ":
+        return BlockType.HEADING
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    for line in lines:
+        if not line.startswith(">"):
+            is_quote = False
+            break
+    if is_quote:
+        return BlockType.QUOTE
+    for line in lines:
+        if not line.startswith("- "):
+            is_ul = False
+            break
+    if is_ul:
+        return BlockType.UNORDERED_LIST
+    for line in lines:
+        if not line.startswith(f"{i}. "):
+            is_ol = False
+            break
+        else:
+            i += 1
+    if is_ol:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
 def markdown_to_blocks(text: str) -> list[str]:
     blocks = []
     splitted_text = text.split("\n\n")
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -65,3 +65,76 @@ This is a **bolded** paragraph
                 "This is single block of the markdown document",
             ]
         )
+
+    def test_paragraph(self):
+        block = "This is just a normal paragraph of text."
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_heading(self):
+        block = "###### This is 6th heading"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_code(self):
+        block = """```
+        This is a code block
+        ```"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_quote(self):
+        block = "> This is a quote"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_ul(self):
+        block = "- This is an unordered list"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+
+    def test_ol(self):
+        block = "1. This is an ordered list"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+    def test_seven_heading(self):
+        block = "####### This is 7th heading but it's not heading"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_not_same_type(self):
+        block = """> This is a quote
+- This is not a quote"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_skipped_ordered_list(self):
+        block = """1. This is first element of the ordered list
+3. This is 3rd element not 2nd"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_missing_backticks(self):
+        block = """```
+        This a code block
+        ``"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_one_heading(self):
+        block = "# This is 1st Heading"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_start_at_second_ordered_list(self):
+        block = """2. This starts at 2nd
+3. This is another element"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_start_at_first_ordered_list(self):
+        block = """1. This starts at 2nd
+2. This is another element"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
