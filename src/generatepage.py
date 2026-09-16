fd6c19df641766244be25d7a2a720d456c42c118
Jakub Balys <kbalys444@gmail.com>
2026-09-16 19:32:59 +0200

    N: Generating the pages recursively

--- src/generatepage.py
+++ src/generatepage.py
@@ -1,6 +1,7 @@
 from markdown_blocks import markdown_to_html_node
 from htmlnode import HTMLNode
 import os
+from pathlib import Path
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -25,3 +26,13 @@ def generate_page(from_path, template_path, dest_path):
     os.makedirs(destination_directory, exist_ok = True)
     with open(dest_path, 'w') as file:
         page = file.write(page)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for content in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, content)
+        dest_path = os.path.join(dest_dir_path, content)
+        if not os.path.isfile(from_path):
+            generate_pages_recursive(from_path, template_path, dest_path)
+        else:
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from copystatic import ensure_clean_directory, copy_directory_to_another
-from generatepage import generate_page
+from generatepage import generate_pages_recursive
 
 def main():
     ensure_clean_directory("public")
     copy_directory_to_another("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
