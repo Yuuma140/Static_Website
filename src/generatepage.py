a6e15d4dd594175554e1c33e7b610a597713bdc0
Jakub Balys <kbalys444@gmail.com>
2026-09-17 19:13:31 +0200

    O: fixed the .nojekyll bug

--- src/generatepage.py
+++ src/generatepage.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
             return title
     raise Exception("no title found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as file:
         from_path_content = file.read()
@@ -22,17 +22,19 @@ def generate_page(from_path, template_path, dest_path):
     page = template_path_content
     page = page.replace("{{ Title }}", title)
     page = page.replace("{{ Content }}", from_path_html)
+    page = page.replace('href="/', f'href="{basepath}')
+    page = page.replace('src="/', f'src="{basepath}')
     destination_directory = os.path.dirname(dest_path)
     os.makedirs(destination_directory, exist_ok = True)
     with open(dest_path, 'w') as file:
         page = file.write(page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for content in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, content)
         dest_path = os.path.join(dest_dir_path, content)
         if not os.path.isfile(from_path):
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
         else:
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from copystatic import ensure_clean_directory, copy_directory_to_another
 from generatepage import generate_pages_recursive
+import sys
 
 def main():
-    ensure_clean_directory("public")
-    copy_directory_to_another("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    ensure_clean_directory("docs")
+    copy_directory_to_another("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
